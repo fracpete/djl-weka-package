66feb1413b81a4d1217b612e3e67a556df718211
Peter Reutemann <fracpete@gmail.com>
2025-09-23 13:23:30 +1200

    improved exception handling

--- src/main/java/weka/classifiers/djl/DJLUtils.java
+++ src/main/java/weka/classifiers/djl/DJLUtils.java
@@ -25,6 +25,8 @@ import ai.djl.pytorch.engine.PtEngineProvider;
 import ai.djl.util.ClassLoaderUtils;
 import weka.core.WekaPackageClassLoaderManager;
 
+import java.lang.reflect.Method;
+
 /**
  * Utility functions for DJL.
  *
@@ -32,15 +34,57 @@ import weka.core.WekaPackageClassLoaderManager;
  */
 public class DJLUtils {
 
+  private static boolean ClassLoaderUtilsExceptionShown = false;
+
+  private static boolean EngineExceptionShown = false;
+
   /**
    * Initialize the classloader using the current context.
    *
-   * @param context	the object trying to use DJL
+   * @param context	the object trying to use DJL, like a classifier
    */
   public static void initClassLoader(Object context) {
-    ClassLoader wekaClassLoader = WekaPackageClassLoaderManager.getWekaPackageClassLoaderManager().getLoaderForClass(context.getClass().getName());
-    ClassLoaderUtils.setContextClassLoader(wekaClassLoader);
-    Engine.reInitEngine(wekaClassLoader);
+    ClassLoader wekaClassLoader;
+    Method	method;
+
+    // get classloader from context
+    wekaClassLoader = WekaPackageClassLoaderManager.getWekaPackageClassLoaderManager().getLoaderForClass(context.getClass().getName());
+
+    // ClassLoaderUtils
+    try {
+      // ClassLoaderUtils.setContextClassLoader(wekaClassLoader);
+      method = ClassLoaderUtils.class.getMethod("setContextClassLoader", ClassLoader.class);
+      method.invoke(null, wekaClassLoader);
+    }
+    catch (NoSuchMethodException ne) {
+      if (!ClassLoaderUtilsExceptionShown) {
+	ClassLoaderUtilsExceptionShown = true;
+	System.err.println("If you are using DJL via Maven, you can ignore the following exception:");
+	ne.printStackTrace();
+      }
+    }
+    catch (Exception e) {
+      System.err.println("Something unexpected went wrong:");
+      e.printStackTrace();
+    }
+
+    // Engine
+    try {
+      // Engine.reInitEngine(wekaClassLoader);
+      method = Engine.class.getMethod("reInitEngine", ClassLoader.class);
+      method.invoke(null, wekaClassLoader);
+    }
+    catch (NoSuchMethodException ne) {
+      if (!EngineExceptionShown) {
+	EngineExceptionShown = true;
+	System.err.println("If you are using DJL via Maven, you can ignore the following exception:");
+	ne.printStackTrace();
+      }
+    }
+    catch (Exception e) {
+      System.err.println("Something unexpected went wrong:");
+      e.printStackTrace();
+    }
   }
 
   /**
